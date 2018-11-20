f49a92109ea8c2fc410a2313e0bbf33aba8c9124
kupavtsev <kupavtsev@ctvmo.local>
2018-11-20 11:32:31 +0300

    Запустилось

--- include/gui_basis/mainwindow.h
+++ include/gui_basis/mainwindow.h
@@ -3,13 +3,21 @@
 
 #include <QMainWindow>
 
+#ifndef GUI_BASIS_EXPORT
+#if defined(gui_basis_EXPORTS)
+#define GUI_BASIS_EXPORT __declspec(dllexport)
+#else
+#define GUI_BASIS_EXPORT __declspec(dllimport)
+#endif
+#endif
+
 namespace Ui
 {
 	class MainWindow;
 }
 
 
-class CMainWindow : public QMainWindow
+class GUI_BASIS_EXPORT CMainWindow : public QMainWindow
 {
 	Q_OBJECT
 
